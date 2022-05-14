725e2458b13b18ec9a6d1773d05408ab573bfc7b
macmeals <loseworld9731@gmail.com>
2022-05-14 17:07:47 +0900

    Page系のコンポーネントの作成及びCSSライブラリーのEmotionを導入

---
+++ src/component/pages/TopPage.tsx
@@ -0,0 +1,27 @@
+import { css } from "@emotion/react";
+
+export const TopPage = () => {
+  // const topStyle = css`
+  //   display: flex;
+  //   justify-content: center;
+  //   flex-direction: column;
+  //   align-items: center;
+  // `;
+
+  //TypeScriptでEmotionのCSSを記載。
+  const topStyles = css({
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+  });
+
+  return (
+    <div>
+      {/* <div css={topStyle}> */}
+      <div css={topStyles}>
+        <h1>Todoアプリ</h1>
+      </div>
+    </div>
+  );
+};
